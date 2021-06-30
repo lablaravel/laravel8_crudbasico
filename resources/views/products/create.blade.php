d8ccc81be7fda5e3457cb98e849b4a32b0ad9eb8
Pwcodigo <pwcodigo@gmail.com>
2021-06-30 14:10:59 -0300

    feat: funcionado cadastro de produto e exibicao dele

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,47 @@
+@extends('products.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2> Add New Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn primary" href="{{ route ('products.index')}}"><<< Voltar</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <p>
+            <strong>ERRO! </strong> Você está tendo problemas.
+        </p>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('products.store') }}" method="POST">
+    @csrf
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nome: </strong>
+                    <input tpye="text" name="name" class="form-control" placeholder="name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Detalhes:<strong>
+                    <textarea class="form-control" style="height:150px" name="detail" placeholder="Detalhes do produto"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+</form>
+@endsection
